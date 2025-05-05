1298a702f18bdcd97e75f1720f6982d9c9aad60c
isratRithy <israt.rithy@bluetree.no>
2025-05-05 15:55:50 +0600

    setting up tags model

--- app/core/models.py
+++ app/core/models.py
@@ -56,6 +56,20 @@ class Recipe(models.Model):
     price = models.DecimalField(max_digits=5, decimal_places=2)
     link = models.CharField(max_length=255, blank=True)
     date_added = models.DateTimeField(auto_now_add=True)
+    tags = models.ManyToManyField('Tag', blank=True)
 
     def __str__(self):
         return self.title
+    
+
+class Tag(models.Model):
+    """Tag for filtering recipes."""
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+    name = models.CharField(max_length=255)
+    
+
+    def __str__(self):
+        return self.name
